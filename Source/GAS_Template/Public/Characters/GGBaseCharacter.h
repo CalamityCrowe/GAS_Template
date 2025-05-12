9afead1dcf06a77ad33b8c12c042ae835eccaffb
Kieran T <kieran123416@gmail.com>
2025-05-12 23:39:40 +0100

    GAS blueprint implementations

--- Source/GAS_Template/Public/Characters/GGBaseCharacter.h
+++ Source/GAS_Template/Public/Characters/GGBaseCharacter.h
@@ -53,7 +53,7 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-private:
+protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS", meta = (AllowPrivateAccess = true))
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS", meta = (AllowPrivateAccess = true))
